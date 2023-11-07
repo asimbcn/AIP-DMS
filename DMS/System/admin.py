00cce204ae5a13b4bbdbf246773494f22a5388d6
Asim Raj Pandey <asimbcn@gmail.com>
2023-11-07 14:43:52 -0500

    Bug fixes

--- DMS/System/admin.py
+++ DMS/System/admin.py
@@ -4,5 +4,5 @@ from .models import *
 # Register your models here.
 admin.site.register(Files)
 admin.site.register(Version_control)
-# admin.site.register(Temp_OCR)
+admin.site.register(Temp_OCR)
 # admin.site.register(Security_logs)
